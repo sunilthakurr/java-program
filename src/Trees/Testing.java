006a23ddf2c4e71ed05587e0be13f4ffff3eee99
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-11-06 21:32:57 +0530

    Faltu

---
+++ src/Trees/Testing.java
@@ -0,0 +1,10 @@
+package Trees;
+
+import java.util.Scanner;
+
+public class Testing {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Heloo everyone ");
+    }
+}
