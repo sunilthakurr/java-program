f9a15e278b6a5970b70698ac1dab593b0a4471ac
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-11-09 15:31:46 +0530

    displaying our trees for basic understanding

---
+++ src/Trees/TreeUse.java
@@ -0,0 +1,18 @@
+package Trees;
+
+public class TreeUse {
+    public static void main(String[] args) {
+        TreeNode<Integer> root = new TreeNode<>(4);
+        TreeNode<Integer> node1 = new TreeNode<>(2);
+        TreeNode<Integer> node2 = new TreeNode<>(3);
+        TreeNode<Integer> node3 = new TreeNode<>(9);
+        TreeNode<Integer> node4 = new TreeNode<>(6);
+        TreeNode<Integer> node5 = new TreeNode<>(7);
+        root.children.add(node1);
+        root.children.add(node2);
+        root.children.add(node3);
+        node2.children.add(node4);
+        node2.children.add(node5);
+
+    }
+}
