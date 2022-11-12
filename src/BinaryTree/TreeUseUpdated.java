7ae1f988610ea86a519af9ec13750f1a9128a240
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-11-12 19:54:22 +0530

    Use input-output class written iteratively(easy to understand way)

---
+++ src/BinaryTree/TreeUseUpdated.java
@@ -0,0 +1,9 @@
+package BinaryTree;
+
+public class TreeUseUpdated {
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> root = TakeInputLevelWise.takeInputLevelWise();
+        PrintLevelWise.printLevelWise(root);
+
+    }
+}
