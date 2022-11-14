b4b2e1869827d72d7b8ded69313a232e656082f5
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-11-14 23:07:39 +0530

    Update SpiralPrintAntiClockWise.java

--- src/GLA_Super_150/Assignment3/SpiralPrintAntiClockWise.java
+++ src/GLA_Super_150/Assignment3/SpiralPrintAntiClockWise.java
@@ -21,26 +21,12 @@ public class SpiralPrintAntiClockWise {
                                         int n,
                                         int arr[][]) {
         int i, k = 0, l = 0;
-
-    /* k - starting row index
-        m - ending row index
-        l - starting column index
-        n - ending column index
-        i - iterator */
-
-        // initialize the count
         int cnt = 0;
-
-        // total number of
-        // elements in matrix
         int total = m * n;
 
         while (k < m && l < n) {
             if (cnt == total)
                 break;
-
-            // Print the first column
-            // from the remaining columns
             for (i = k; i < m; ++i) {
                 System.out.print(arr[i][l] + ", ");
                 cnt++;
@@ -49,9 +35,6 @@ public class SpiralPrintAntiClockWise {
 
             if (cnt == total)
                 break;
-
-            // Print the last row from
-            // the remaining rows
             for (i = l; i < n; ++i) {
                 System.out.print(arr[m - 1][i] + ", ");
                 cnt++;
@@ -60,9 +43,6 @@ public class SpiralPrintAntiClockWise {
 
             if (cnt == total)
                 break;
-
-            // Print the last column
-            // from the remaining columns
             if (k < m) {
                 for (i = m - 1; i >= k; --i) {
                     System.out.print(arr[i][n - 1] + ", ");
@@ -73,9 +53,6 @@ public class SpiralPrintAntiClockWise {
 
             if (cnt == total)
                 break;
-
-            // Print the first row
-            // from the remaining rows
             if (l < n) {
                 for (i = n - 1; i >= l; --i) {
                     System.out.print(arr[k][i] + ", ");
