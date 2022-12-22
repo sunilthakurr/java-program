4f87dd922532d382f63e85d5280f76fea4f60aff
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-12-22 20:41:53 +0530

    chegg doubt

---
+++ src/Arrays/TrialAndTesting.java
@@ -0,0 +1,10 @@
+package Arrays;
+
+public class TrialAndTesting {
+    public static void main(String[] args) {
+        int g = 3;
+        System.out.println(++g * g);
+//        re(25);
+    }
+//    public static int fun(int x)
+}
