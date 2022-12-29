c8a20bf63e95a0156c25652661886aa1dab02c78
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2022-12-29 17:49:32 +0530

    Hashmap implementation

---
+++ src/HashMap/OurMapUse.java
@@ -0,0 +1,11 @@
+package HashMap;
+
+public class OurMapUse {
+    public static void main(String[] args) {
+        Map<String, Integer> map = new Map<>();
+        for (int i = 0; i < 20; ++i) {
+            map.insert("abc" + i, 1 + i);
+            System.out.println("i = " + i + " Load Factor = " + map.loadFactor());
+        }
+    }
+}
