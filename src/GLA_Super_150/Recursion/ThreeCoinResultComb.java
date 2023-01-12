cba43c58318609d0dadc6691122de0027ee702f4
SUNIL KUMAR <91265823+sunilthakurr@users.noreply.github.com>
2023-01-12 09:03:32 +0530

    Display all the coin possibilities on tossing N coins

---
+++ src/GLA_Super_150/Recursion/ThreeCoinResultComb.java
@@ -0,0 +1,39 @@
+package GLA_Super_150.Recursion;
+
+import java.util.Scanner;
+
+public class ThreeCoinResultComb {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        System.out.println("Enter the number of coins");
+        int N = obj.nextInt();
+        CoinCombinationWithout(N, "");
+        System.out.println();
+        CoinCombination(N, "");
+
+
+    }
+
+    // method to display all the possibilities without concurrent heads
+    public static void CoinCombinationWithout(int N, String helper) {
+
+        if (N == 0) {
+            if (!helper.contains("HH"))
+                System.out.print(helper + " ");
+            return;
+        }
+
+        CoinCombinationWithout(N - 1, helper + "H");
+        CoinCombinationWithout(N - 1, helper + "T");
+    }
+
+    // displaying all the coin possibilities
+    public static void CoinCombination(int N, String helper) {
+        if (N == 0) {
+            System.out.print(helper + " ");
+            return;
+        }
+        CoinCombination(N - 1, helper + "H");
+        CoinCombination(N - 1, helper + "T");
+    }
+}
